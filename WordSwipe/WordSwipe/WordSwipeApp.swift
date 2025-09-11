bd5f0e78f2d8fccd4ac3d33f4ef981b704e896fd
Philipp Lazarev <philipp.lazareff@gmail.com>
2025-09-11 15:28:18 +0300

    Starting from scratch

---
+++ WordSwipe/WordSwipe/WordSwipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordSwipeApp.swift
+//  WordSwipe
+//
+//  Created by Philipp Lazarev on 11.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct WordSwipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
